b83ad69ecfd8507d57b1f0223202e759cf896fdb
jbennett122 <justinbennett122@yahoo.com>
2015-04-14 18:22:03 -0400

    .

--- src/solvers/ge.cpp
+++ src/solvers/ge.cpp
@@ -21,119 +21,87 @@ using namespace std;
 
 void gaussianElim(int argc,char* argv[])
 {
-ifstream fs;
 
+ifstream fs;
 	fs.open(argv[1]);
 
-	 string test;
+	int i,j,k,l,m,n,count,choice;
+	double nums,sum,tolerance,max;
+	double error;
+	count=0;
 
-	 //stream variable
-
-	 ifstream fs2;
-	 //is the file found?
-	 if(argc != 3){
-	 		cout<<"Files not found"<<endl;
-	 		exit(0);
-	 	}
+	fs.clear();
+	fs>>n>>m;
 
 
-	   if (!fs.is_open())
-	   {
-	     cout << "Operation not successfully performed\n";
-	     fs.close();
-	   }
+	double **A,**D,**C,*x,*old,*diff;	//A D matrix
+	double b[n];	//Right hand side of Ax=b
+	double ending=1.0/10000.0;
 
-	for(int i=0;i<argc;i++)
-		 		printf("Argument %d: %s\n", (i), argv[i]);
+	srand (time(NULL));
 
 
-		fs2.open(argv[2]);
+	//initial guess
+		cout<<"\nInitial guess"<<endl;
+		x=new double [n];
+		for(i=0;i<n;i++){
+
+			double v1 = rand() %200 -100;
+
+			x[i]=v1;
+		}
+				for(i=0;i<n;i++){
+						cout<<"|";
+							cout<<" "<<setw(8)<<x[i]<<" ";
+						cout<<"|"<<endl;
+							}
 
 
-		//create random matrix
+		//create matrix and import values
+			A = new double *[n];
+			for (i=0; i < n; i++)
+			{
+				A[i]=new double [m];
 
-int r1=0;
-int r2=1;
-
-		int i,j,k,l,m,n,count,choice;
-		double nums,sum,tolerance,max;
-		double error;
-		count=0;
-
-		fs.clear();
-		fs>>n>>m;
-
-
-cout<<"test"<<endl;
-		double **A,**D,**C,*x,*old,*diff;	//A D matrix
-		double b[n];	//Right hand side of Ax=b
-		double ending=1.0/10000.0;
-
-		srand (time(NULL));
-
-
-		//initial guess
-			cout<<"\nInitial guess"<<endl;
-			x=new double [n];
-			for(i=0;i<n;i++){
-
-				double v1 = rand() %200 -100;
-
-				x[i]=v1;
-			}
-					for(i=0;i<n;i++){
-							cout<<"|";
-								cout<<" "<<setw(8)<<x[i]<<" ";
-							cout<<"|"<<endl;
-								}
-
-
-			//create matrix and import values
-				A = new double *[n];
-				for (i=0; i < n; i++)
-				{
-					A[i]=new double [m];
-
-					for(j=0;j<m;j++){
-
-						fs>>nums;
-						A[i][j]=nums;
-						count++;
-
-					}
-				}
-
-				cout<<"\nA matrix "<<endl;
-			 	for (i=0; i < n; i++)
-					{
-					cout<<"|";
-						for(j=0;j<m;j++){
-
-						  cout<<" "<<setw(8)<<A[i][j]<<" ";
-						}
-					cout<<"|"<<endl;
-					}
-
-
-				//cout<<"entries in A matrix: "<<count<<endl;
-				cout<<"\nRHS"<<endl;
-
-				count =0;
-
-			for(i=0;i<n;i++){
+				for(j=0;j<m;j++){
 
 					fs>>nums;
-					b[i]=nums;
+					A[i][j]=nums;
 					count++;
+
+				}
+			}
+
+			cout<<"\nA matrix "<<endl;
+		 	for (i=0; i < n; i++)
+				{
+				cout<<"|";
+					for(j=0;j<m;j++){
+
+					  cout<<" "<<setw(8)<<A[i][j]<<" ";
 					}
+				cout<<"|"<<endl;
+				}
 
-				for (i=0; i < n; i++)
-						{
-						cout<<"|";
-							cout<<" "<<setw(8)<<b[i]<<" ";
-						cout<<"|"<<endl;
-						}
 
+			//cout<<"entries in A matrix: "<<count<<endl;
+			cout<<"\nRHS"<<endl;
+
+			count =0;
+
+		for(i=0;i<n;i++){
+
+				fs>>nums;
+				b[i]=nums;
+				count++;
+				}
+
+			for (i=0; i < n; i++)
+					{
+					cout<<"|";
+						cout<<" "<<setw(8)<<b[i]<<" ";
+					cout<<"|"<<endl;
+					}
 
 
 		/*		rowSwap( A,  r1, r2);
